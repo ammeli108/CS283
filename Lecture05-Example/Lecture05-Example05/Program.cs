1225d01c4f985d66990b2300490673a46cc629c9
ammeli108 <ammeako@gmail.com>
2017-07-26 10:03:11 +0800

    上傳Lecture05-Eample

---
+++ Lecture05-Example/Lecture05-Example05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture05_Example05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+            int total = 0;
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                numbers[index] = random.Next(-1000, 1001);
+            }
+
+            foreach (int number in numbers)
+            {
+                total += number; // 程式只有一行, {}其實可以不用寫
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Lecture05-Example/Lecture05-Example06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture05_Example06
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // int next = 1;
+            int next = 0;
+            int total = 0;
+            while (total < 10)
+            {
+                total += (next++);
+            }
+            Console.WriteLine("next =" + next);
+            Console.WriteLine("total =" + total);
+        }
+    }
+}
